fae17c98e4b804122e83856196535ef320066268
Kai Windle <kaiwindle@gmail.com>
2024-05-07 20:57:58 +0100

    Templates for source code blocks added

--- modules/kw-org.el
+++ modules/kw-org.el
@@ -5,5 +5,18 @@
   :hook (org-mode . org-bullets-mode)
   :custom (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
+;; This is needed as of Org 9.2
+;; If this isn't installed try Alt-x list packages
+;; and install 9.4 at the time of writing
+;; Note if you want to use begin_example use left arrow symbol and e then tab to auto complete
+(with-eval-after-load 'org
+  (require 'org-tempo)
+  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist '("py" . "src python"))
+  (add-to-list 'org-structure-template-alist '("go" . "src go"))
+  (add-to-list 'org-structure-template-alist '("yaml" . "src yaml"))
+  (add-to-list 'org-structure-template-alist '("json" . "src json"))
+  (add-to-list 'org-structure-template-alist '("gq" . "src graphql")))
 
 (provide 'kw-org)
