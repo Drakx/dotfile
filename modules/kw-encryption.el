481a31e5560d4c55dcfe418fa404209264ec9ad9
Kai Windle <kaiwindle@gmail.com>
2024-05-07 13:21:52 +0100

    gpg path updated with linux path

--- modules/kw-encryption.el
+++ modules/kw-encryption.el
@@ -8,8 +8,12 @@
   ;; 'silent to use symmetric encryption
   ;; nil to ask for users unless specified
   ;; t to always ask for a user
-  (when (eq system-type 'darwin)
-    (custom-set-variables '(epg-gpg-program "/usr/local/MacGPG2/bin/gpg2")))
+  (when (or (eq system-type 'darwin)
+	    (eq system-type 'gnu/linux))
+    (custom-set-variables '(epg-gpg-program
+			    (if (eq system-type 'darwin)
+				"/usr/local/MacGPG2/bin/gpg2"
+			      "/usr/bin/gpg"))))
   (epa-file-select-keys nil))
 
 (use-package org-crypt
